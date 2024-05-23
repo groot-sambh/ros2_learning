531145911177e29d9e3dc133cc1d2018fe97194e
Sambhav <sambhavparmar84@gmail.com>
2024-05-23 12:44:12 +0530

    queuing goals

--- src/action_py/action_py/count_until_server.py
+++ src/action_py/action_py/count_until_server.py
@@ -15,33 +15,54 @@ class CountUntilServerNode(Node):
         super().__init__("count_until_server")
         self.goal_handle_ : ServerGoalHandle = None
         self.goal_lock_ = threading.Lock()
+        self.goal_queue = []
         self.count_until_server_ = ActionServer(self, 
                                                 CountUntil, 
                                                 "count_until", 
                                                 goal_callback=self.goal_callback,
+                                                handle_accepted_callback=self.handle_accepted_callback,
                                                 cancel_callback=self.cancel_callback,
                                                 execute_callback=self.execute_callback,
                                                 callback_group=ReentrantCallbackGroup())
         self.get_logger().info("The count until server has been started")
 
-    def cancel_callback(self, goal_handle: ServerGoalHandle):
-        return CancelResponse.ACCEPT #or REJECT
+    
+
+    
     
     def goal_callback(self, goal_request: CountUntil.Goal):
         self.get_logger().info("Recieved a Goal")
 
         #Policy refuse new goal if current goal still active
-        with self.goal_lock_:
-            if self.goal_handle_ is not None and self.goal_handle_.is_active:
-                self.get_logger().info("A goal is already active, rejecting new goal")
-                return GoalResponse.REJECT
+        # with self.goal_lock_:
+        #     if self.goal_handle_ is not None and self.goal_handle_.is_active:
+        #         self.get_logger().info("A goal is already active, rejecting new goal")
+        #         return GoalResponse.REJECT
+
         # Validate the goal
         if goal_request.target_number <= 0:
             self.get_logger().info("Rejecting the goal")
             return GoalResponse.REJECT
+        
+        # Policy: preempt existing goal when recieving new goal
+        # with self.goal_lock_:
+        #     if self.goal_handle_ is not None and self.goal_handle_.is_active:
+        #         self.get_logger().info("abort current goal and accept new goal")
+        #         self.goal_handle_.abort()
+
         self.get_logger().info("Accepting the goal")
         return GoalResponse.ACCEPT
+    
+    def handle_accepted_callback(self,goal_handle: ServerGoalHandle):
+        with self.goal_lock_:
+            if self.goal_handle_ is not None:
+                self.goal_queue.append(goal_handle)
+            else:
+                goal_handle.execute()
 
+    def cancel_callback(self, goal_handle: ServerGoalHandle):
+        return CancelResponse.ACCEPT #or REJECT
+    
     def execute_callback(self, goal_handle: ServerGoalHandle):
         with self.goal_lock_:
             self.goal_handle_ = goal_handle
@@ -56,6 +77,10 @@ class CountUntilServerNode(Node):
         counter = 0
         result = CountUntil.Result()
         for i in range(target_number):
+            if not goal_handle.is_active:
+                result.reached_number = counter
+                self.process_next_goal_in_queue
+                return result
             if goal_handle.is_cancel_requested:
                 self.get_logger().info("Cancelling the goal!")
                 goal_handle.canceled()
@@ -68,12 +93,19 @@ class CountUntilServerNode(Node):
             time.sleep(period)
 
         #set goal in final state
-        goal_handle.abort()
+        goal_handle.succeed()
 
         #send the result
-        result = CountUntil.Result()
         result.reached_number = counter
+        self.process_next_goal_in_queue
         return result
+    
+    def process_next_goal_in_queue(self):
+        with self.goal_lock_:
+            if len(self.goal_queue) > 0:
+                self.goal_queue.pop(0).execute()
+            else:
+                self.goal_handle_ = None
 
 def main(args=None):
     rclpy.init(args=args)
