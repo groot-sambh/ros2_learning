3a10bda1c7eac74096ea8d8415f5252031a2e028
Sambhav <sambhavparmar84@gmail.com>
2024-06-08 16:31:01 +0530

    manual composition

--- src/components_py/setup.py
+++ src/components_py/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "manual_compositon = components_py.manual_composition:main"
         ],
     },
 )
