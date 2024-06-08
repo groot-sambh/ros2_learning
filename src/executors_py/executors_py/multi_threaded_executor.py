2f198c609734ee97e8c6bffcaced808e31081091
Sambhav <sambhavparmar84@gmail.com>
2024-06-08 15:47:03 +0530

    multiple nodes 2

--- src/executors_py/executors_py/multi_threaded_executor.py
+++ src/executors_py/executors_py/multi_threaded_executor.py
@@ -8,17 +8,14 @@ from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallb
 class Node1(Node):
     def __init__(self):
         super().__init__("node1")
-        self.cb_group1_ = ReentrantCallbackGroup()
+        self.cb_group1_ = MutuallyExclusiveCallbackGroup()
         self.cb_group2_ = MutuallyExclusiveCallbackGroup()
         self.timer1_ = self.create_timer(
-            1.0, self.callback_timer1, callback_group=self.cb_group2_)
-        self.get_logger().info('Subscription 1 initialized')
+            1.0, self.callback_timer1, callback_group=self.cb_group1_)
         self.timer2_ = self.create_timer(
-            1.0, self.callback_timer2, callback_group=self.cb_group1_)
-        self.get_logger().info('Subscription 2 initialized')
+            1.0, self.callback_timer2, callback_group=self.cb_group2_)
         self.timer3_ = self.create_timer(
-            1.0, self.callback_timer3, callback_group=self.cb_group1_)
-        self.get_logger().info('Subscription 3 initialized')
+            1.0, self.callback_timer3, callback_group=self.cb_group2_)
 
     def callback_timer1(self):
         time.sleep(2.0)
@@ -32,12 +29,31 @@ class Node1(Node):
         time.sleep(2.0)
         self.get_logger().info("cb 2")
 
+class Node2(Node):
+    def __init__(self):
+        super().__init__("node2")
+        self.cb_group_ = ReentrantCallbackGroup()
+        self.timer2_ = self.create_timer(
+            1.0, self.callback_timer4, callback_group=self.cb_group_)
+        self.timer3_ = self.create_timer(
+            1.0, self.callback_timer5, callback_group=self.cb_group_)
+        
+    def callback_timer4(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 4")
+
+    def callback_timer5(self):
+        time.sleep(2.0)
+        self.get_logger().info("cb 5")
+
 
 def main(args=None):
     rclpy.init(args=args)
     node1 = Node1()
+    node2 = Node2()
     executor = MultiThreadedExecutor()
     executor.add_node(node1)
+    executor.add_node(node2)
     executor.spin()
     rclpy.shutdown()
 
